941b4f660b3f1c494d84840af67b49810528a664
L6he <158562450+L6he@users.noreply.github.com>
2024-10-17 14:20:33 +0300

    add main project jv

---
+++ PirateTARpe23/Controllers/PirateController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PirateTARpe23.Core.Domain;
+using PirateTARpe23.Core.ServiceInterface;
+using PirateTARpe23.Data;
+using PirateTARpe23.Models.Pirates;
+
+namespace PirateTARpe23.Controllers
+{
+    public class PirateController : Controller
+    {
+        private readonly PirateTARpe23Context _context;
+        private readonly IPirateServices _pirateServices;
+
+        public PirateController(PirateTARpe23Context context, IPirateServices pirateServices)
+        {
+            _context = context;
+            _pirateServices = pirateServices;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var result = _context.Pirates
+                .OrderByDescending(y => y.Level)
+                .Select(x => new PirateIndexViewModel
+                {
+                    ID = x.ID,
+
+                    Name = x.Name,
+
+                    Health = x.Health,
+
+                    HungerLevel = x.HungerLevel,
+
+                    ThirstLevel = x.ThirstLevel,
+
+                    Level = x.Level,
+
+                    XP = x.XP
+                }
+                );
+            return View(result);
+        }
+    }
+}
---
+++ PirateTARpe23/Models/Pirates/PirateIndexViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PirateTARpe23.Models.Pirates
+{
+    public enum StatusEffect
+    {
+        Clear,
+        Bleeding,
+        Poisoned,
+        HasScurvy,
+    }
+    public enum PrimaryWeapon
+    {
+        Blunderbuss, Musket, Cannon, FlintlockPistol
+    }
+    public enum SecondaryWeapon
+    {
+        Dagger, Scimitar, Cutlass
+    }
+    public class PirateIndexViewModel
+    {
+        public Guid ID { get; set; }
+        public string? Name { get; set; }
+        public double Health { get; set; }
+        public StatusEffect StatusEffect { get; set; }
+        public double XP { get; set; }
+        public int Level { get; set; }
+        public PrimaryWeapon PrimaryWeapon { get; set; }
+        public SecondaryWeapon SecondaryWeapon { get; set; }
+        public string? Item { get; set; }
+        public double HungerLevel { get; set; }
+        public double ThirstLevel { get; set; }
+    }
+}
