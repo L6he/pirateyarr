1d7556f04ccc34159b3ee312f1aa634787b55222
opilane <opilane@HP-CZC3217068>
2024-10-21 16:30:46 +0300

    jgjghj

---
+++ PirateTARpe23/Models/Pirates/PirateImageViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace PirateTARpe23.Models.Pirates
+{
+    public class PirateImageViewModel
+    {
+        public Guid ImageID { get; set; }
+
+        public string ImageTitle { get; set; }
+
+        public byte[] ImageData { get; set; }
+
+        public string Image { get; set; }
+
+        public Guid? PirateID { get; set; }
+    }
+}
