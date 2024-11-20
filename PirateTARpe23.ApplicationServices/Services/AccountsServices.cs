21aac274f2c1c8a73e7966bd306cdbee7d5177ed
L6he <158562450+L6he@users.noreply.github.com>
2024-11-20 15:21:46 +0200

    IAccountsServices and AccountsServices added

---
+++ PirateTARpe23.ApplicationServices/Services/AccountsServices.cs
@@ -0,0 +1,28 @@
+﻿
+using Microsoft.AspNetCore.Identity;
+using PirateTARpe23.Core.Domain;
+using PirateTARpe23.Core.ServiceInterface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PirateTARpe23.ApplicationServices.Services
+{
+    public class AccountsServices : IAccountsServices
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public AccountsServices
+            (
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager
+            )
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+    }
+}
